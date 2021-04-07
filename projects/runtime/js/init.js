6f3ba000f97df84043d8053b6b411e7879387d2a
cmganus <cmganuswork@gmail.com>
2021-04-07 14:08:25 +0000

    added buildings and tree

--- projects/runtime/js/init.js
+++ projects/runtime/js/init.js
@@ -33,6 +33,8 @@ var init = function (window) {
 
     // TODO 2 : add background
 
+    var background = opspark.makeBackground(app,ground);
+    view.addChild(background);
     
     var help = draw.textfield('MOVES || up: jump | right: flying jump | down: duck | space: fire | q self destruct!', 
         '20px Arial',
@@ -72,7 +74,9 @@ var init = function (window) {
     
     // TODO 1 : add a heads-up display to game
 
-
+    var hud = opspark.makeHud();
+    view.addChild(hud);
+    window.hud = hud;
 
     var game = opspark.createGameManager(app,hud);
     opspark.runLevelInGame(game);
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,8 +26,9 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree;
+        var buildings = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,19 +37,53 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth, groundY,'lightblue');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
-            
+             for (var i = 0; i < 100; i++) {
+                var circle = draw.circle(10,'white','LightGray',2);
+                circle.x = canvasWidth*Math.random();
+                circle.y = groundY*Math.random();
+                background.addChild(circle);
+            }
+
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = -5;
+            moon.y = -5;
+            moon.scaleX = 0.5;
+            moon.scaleY = 0.5;
+            background.addChild(moon);
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+           
+            for(var i=0;i<5;++i) {
+            var buildingHeight = 300;
+            var building = draw.rect(75,buildingHeight,'Yellow','Black',1);
+            building.x = 200*i;
+            building.y = groundY-buildingHeight;
+            background.addChild(building);
+            buildings.push(building);
+            }
+
+
+
+
+
+
+
+
+
+
             
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = 500;
+            tree.y = 100;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
+        
         
         
         // Perform background animation
@@ -60,7 +95,11 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
+            tree.x = tree.x + 1;
+
+            if(tree.x < -200) {
+            tree.x = canvasWidth;
+            }
             
             // TODO 5: Part 2 - Parallax
             
