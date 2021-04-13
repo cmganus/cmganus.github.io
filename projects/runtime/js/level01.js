bf6aeba5ead241696a8230505f913a6729f25041
cmganus <cmganuswork@gmail.com>
2021-04-13 14:14:36 +0000

    for loop

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,6 +19,7 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
+                
             ]
         };
 
@@ -53,13 +54,45 @@ var level01 = function (window) {
 
         }
 
-        createSawBlade(200, 320);
-        createSawBlade(300, 320);
-        createSawBlade(400, 320);
+        createSawBlade(1000, 285);
+        createSawBlade(1800, 285);
+        createSawBlade(2500, 285);
 
-        /* video stopped at 49 minutes. TODO 7 ^^ */
+        
+        for (var i = 0; i < levelData.gameItems.length; i++) {
+          var gameItemObject = levelData.gameItems[i];
+            var firstX = gameItemObject.x;
+            var firstY = gameItemObject.y;
+            var firstType = gameItemObject.type;
+            if (firstType === 'sawblade') {
+                createSawBlade(firstX, firstY); 
 
-        // DO NOT EDIT CODE BELOW HERE
+            }
+            
+           /*  if (gameItemObject.type === 'spikes') {
+                createSawBlade(gameItemObject.x. gameItemObject.y); 
+            }
+            
+            */
+
+        function createSpikes(x,y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);    
+            
+            var obstacleImage = draw.bitmap('img/spikes.png');
+            sawBladeHitZone.addChild(obstacleImage);
+            
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        };
+       
+
+        // DO NOT EDIT CODE BELOW HERE  video stopped at 49 minutes. TODO 7 ^^ //
     }
 };
 
@@ -68,4 +101,5 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = level01;
+}
 }
